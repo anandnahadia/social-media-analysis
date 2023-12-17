2a914e8b61bab51ae6f7b6fd269220a5cd3631ae
Test <test@192.168.1.4>
2023-12-17 20:22:12 +0530

    Removed comments

--- src/db.ts
+++ src/db.ts
@@ -3,12 +3,12 @@ import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize({
   dialect: 'postgres',
-  host: 'localhost', // Update with your PostgreSQL server details
+  host: 'localhost',
   port: 5433,
   username: 'postgres',
   password: '1234',
   database: 'postgres-db',
-  logging: false, // Set to true to see SQL logs
+  logging: false,
 });
 
 export default sequelize;
