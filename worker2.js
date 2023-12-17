e887cc8926bbb77dbebf7694842a8aeaab5f6b52
Test <test@192.168.1.4>
2023-12-17 20:12:18 +0530

    Removed worker files

--- worker2.js
+++
@@ -1,7 +0,0 @@
-// worker2.js
-import { messageQueue } from './dist/messageQueue';
-
-messageQueue.process('postAnalysis', async ({ postContent }) => {
-    // Implement analysis logic for worker 2
-    console.log('Worker 2 - Analysis result:', postContent);
-});
